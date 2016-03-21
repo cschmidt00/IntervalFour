aa47995e1a999f0dd3f7d69df4b368b38ea28c85
unknown <mhesby@summitps.org>
2016-03-21 08:42:44 -0700

    changed set private server to 'private server here'

--- stable.js
+++ stable.js
@@ -1,7 +1,7 @@
 var Bot = require('bot');
 var PF = require('pathfinding');
 var bot = new Bot('YOUR_KEY_HERE', 'training', 'http://vindinium.org'); //Put your bot's code here and change training to Arena when you want to fight others.
-// var bot = new Bot('YOUR_KEY_HERE', 'arena', 'http://52.8.116.125:9000'); //Put your bot's code here and change training to Arena when you want to fight others.
+// var bot = new Bot('YOUR_KEY_HERE', 'arena', 'PRIVATE_SERVER_HERE'); //Put your bot's code here and change training to Arena when you want to fight others.
 var goDir;
 var Promise = require('bluebird');
 Bot.prototype.botBrain = function() {
@@ -9,11 +9,11 @@ Bot.prototype.botBrain = function() {
         _this = bot;
         //////* Write your bot below Here *//////
         //////* Set `myDir` in the direction you want to go and then bot.goDir is set to myDir at the bottom *////////
-        
-        /*                                      * 
+
+        /*                                      *
          * This Code is global data!            *
          *                                      */
-        
+
         // Set myDir to what you want and it will set bot.goDir to that direction at the end.  Unless it is "none"
         var myDir;
         var myPos = [bot.yourBot.pos.x, bot.yourBot.pos.y];
@@ -23,21 +23,21 @@ Bot.prototype.botBrain = function() {
         if(bot.yourBot.id != 2) enemyBots.push(bot.bot2);
         if(bot.yourBot.id != 3) enemyBots.push(bot.bot3);
         if(bot.yourBot.id != 4) enemyBots.push(bot.bot4);
-        
-        
-        /*                                      * 
+
+
+        /*                                      *
          * This Code Decides WHAT to do         *
          *                                      */
         var task;
         task = "freemines";
-        
-        
-        
-        /*                                      * 
+
+
+
+        /*                                      *
          * This Code Determines HOW to do it    *
          *                                      */
-        
-        // This Code find the nearest freemine and sets myDir toward that direction // 
+
+        // This Code find the nearest freemine and sets myDir toward that direction //
         if(task === "freemines") {
             var closestMine = bot.freeMines[0];
             for(i = 0; i < bot.freeMines.length; i++) {
@@ -48,9 +48,9 @@ Bot.prototype.botBrain = function() {
             console.log("Claiming a Free Mine!");
             myDir = bot.findPath(myPos, closestMine);
         }
-        
-        
-        /*                                                                                                                              * 
+
+
+        /*                                                                                                                              *
          * This Code Sets your direction based on myDir.  If you are trying to go to a place that you can't reach, you move randomly.   *
          * Otherwise you move in the direction set by your code.  Feel free to change this code if you want.                            */
         if(myDir === "none") {
@@ -61,9 +61,9 @@ Bot.prototype.botBrain = function() {
         } else {
             bot.goDir = myDir;
         }
-        
-        
-        
+
+
+
         ///////////* DON'T REMOVE ANTYTHING BELOW THIS LINE *//////////////
         resolve();
     });
